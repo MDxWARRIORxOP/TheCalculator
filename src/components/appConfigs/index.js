b30e004f98056d1d5c080b4cae7a1d03889d14d7
kingerious <78227395+MDxWARRIORxOP@users.noreply.github.com>
2022-08-27 19:29:15 +0530

    Initial commit

---
+++ src/components/appConfigs/index.js
@@ -0,0 +1,23 @@
+// import * as React from "react"
+// import ReactDOM from "react-dom"
+import * as uniux from "uniux"
+
+const appConfig = {
+  parentProject: { name: "TheCalculator" },
+  name: "TheCalculator",
+  rootURL: "",
+  design: {
+    themeColour: uniux.colourPacks.green.darker
+  },
+  sections: ["Calculator"],
+
+  pageConfigs: {
+    calc: {
+      name: "Calculator",
+      icon: uniux.icons.faCalculator,
+      url: ""
+    }
+  }
+}
+
+export default appConfig
---
+++ src/pages/index.js
@@ -0,0 +1,47 @@
+import * as uniux from "uniux"
+import * as math from "mathjs"
+import React, { useRef } from "react"
+import appConfig from "../components/appConfigs/index.js"
+
+const IndexPage = () => {
+  const resultRef = useRef(null)
+  const meth = e => {
+    e.preventDefault()
+    try {
+      const result = math.evaluate(e.target[0].value)
+      console.log(result)
+      resultRef.current.textContent = result
+    } catch (error) {
+      console.log(error)
+      resultRef.current.textContent = error
+    }
+    e.target[0].value = ""
+  }
+  setInterval(() => {
+    resultRef.current.textContent =
+      "Click on The Button Above For Math Answers!"
+  }, 10000)
+  return (
+    <uniux.Main
+      pageType="columnedApp"
+      appConfig={appConfig}
+      page={appConfig.pageConfigs.calc}
+    >
+      <h1>Welcome to The Calculator!</h1>
+      {/* TODO: 
+      <div>
+        <h1>History:</h1>
+      </div> */}
+      <form onSubmit={meth}>
+        <input type="text" placeholder="1 + 1" required />
+        <button type="submit">Check Answer</button>
+      </form>
+      <br />
+      <div>
+        <h1 ref={resultRef}>Click on The Button Above For Math Answers!</h1>
+      </div>
+    </uniux.Main>
+  )
+}
+
+export default IndexPage
