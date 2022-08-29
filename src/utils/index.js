c47d3daa04c3d1c68fbaa3c44e7b7ecc1ed6460a
kingerious <78227395+MDxWARRIORxOP@users.noreply.github.com>
2022-08-29 15:01:40 +0530

    fix firebase stuff

--- src/utils/index.js
+++ src/utils/index.js
@@ -19,29 +19,36 @@ const db = firestore.getFirestore(app);
  *  @param {String} docName String, name of the document to add data to.
  *  @param {object} dataObj JS object, the data to add to the document.
  *  @example await addData("users", "test", {hello: "world"})
- *  @returns data Reference
  **/
 const addData = async (collectionName, docName, dataObj) => {
-  const dataRef = db.collection(collectionName).doc(docName);
-
-  const dataSet = await dataRef.set(dataObj);
-  console.log(dataSet);
-  return dataSet;
+  try {
+    await firestore.setDoc(firestore.doc(db, collectionName, docName), dataObj);
+    return true;
+  } catch (error) {
+    console.log("error:", error);
+    return false;
+  }
 };
 
 /**
  *  @param {String} collectionName String, name of the collection to get a doc from.
  *  @param {String} docName String, name of the doc ot get data from.
  *  @example getData("users", "Kingerious")
- *  @returns if no data is found, returns null, if data is found, returns the document data as a JS Object
+ *  @returns if no data is found, returns false, if data is found, returns the document data as a JS Object
  **/
 const getData = async (collectionName, docName) => {
-  const dataRef = db.collection(collectionName).doc(docName);
-  const doc = await dataRef.get();
-  if (!doc.exists) {
-    return null;
-  } else {
-    return doc.data();
+  try {
+    const docRef = firebase.doc(db, collectionName, docName);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      return docSnap.data();
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.log("error:", error);
+    return false;
   }
 };
 
